406208b32cfa45c52b0f65be896a93d9e117485e
Zack Jackson <z.jackson58@gmail.com>
2019-01-21 13:29:32 -0500

    firebase setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import App from './components/App';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 ReactDOM.render(
   <Router>
     <Switch>
       <Route path="/" exact component={App} />
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
     </Switch>
   </Router>,
   document.getElementById('root')
