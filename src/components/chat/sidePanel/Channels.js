a405c9678f5841641a1aa2c1e05ec55dab593e26
Zack Jackson <z.jackson58@gmail.com>
2019-01-22 11:56:31 -0500

    Sidepanel user and channels components

--- src/components/chat/sidePanel/Channels.js
+++ src/components/chat/sidePanel/Channels.js
@@ -1,24 +1,70 @@
 import React, { Component } from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 
 class Channels extends Component {
   state = {
-    channels: []
+    channels: [],
+    channelName: '',
+    channelDetails: '',
+    modalOpen: false
+  };
+
+  openModal = () => {
+    this.setState({ modalOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalOpen: false });
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    console.log(e.target, name, value);
+
+    this.setState({ [name]: value });
   };
 
   render() {
-    const { channels } = this.state;
+    const { channels, modalOpen } = this.state;
 
     return (
-      <Menu.Menu>
-        <Menu.Item>
-          <span>
-            <Icon name="exchange" />
-            Channels
-          </span>
-          ({channels.length}) <Icon name="add" />
-        </Menu.Item>
-      </Menu.Menu>
+      <>
+        <Menu.Menu>
+          <Menu.Item>
+            <span>
+              <Icon name="exchange" />
+              Channels
+            </span>
+            ({channels.length}) <Icon name="add" onClick={this.openModal} />
+          </Menu.Item>
+        </Menu.Menu>
+
+        <Modal basic open={modalOpen} onClose={this.closeModal}>
+          <Modal.Header>Add a channel</Modal.Header>
+
+          <Modal.Content>
+            <Form>
+              <Form.Field>
+                <Input fluid label="Channel Name" name="channelName" />
+              </Form.Field>
+
+              <Form.Field>
+                <Input fluid label="About the Channel" name="channelDetails" />
+              </Form.Field>
+            </Form>
+          </Modal.Content>
+
+          <Modal.Actions>
+            <Button color="teal" inverted>
+              <Icon name="checkmark" /> Add
+            </Button>
+
+            <Button color="red" inverted onClick={this.closeModal}>
+              <Icon name="remove" /> Cancel
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      </>
     );
   }
 }
