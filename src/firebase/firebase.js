12f6e176ad9697de75aa3b1c19fbe13d7431aba6
Zack Jackson <z.jackson58@gmail.com>
2019-02-07 22:36:42 -0500

    Favicon

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/storage';
 
 // Initialize Firebase
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: 'AIzaSyCu752QU_-sZMImYyV2H-ff8UUoOiAWsv8',
+  authDomain: 'slackr-cb2a2.firebaseapp.com',
+  databaseURL: 'https://slackr-cb2a2.firebaseio.com',
+  projectId: 'slackr-cb2a2',
+  storageBucket: 'slackr-cb2a2.appspot.com',
+  messagingSenderId: '424072241461'
 };
 
 firebase.initializeApp(config);
